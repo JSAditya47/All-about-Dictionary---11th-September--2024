29fb9df8c3df8c03c6e8eb2cdb6bc2958aeb4b6c
Jyotirmoy Saha Aditya <adityajyotirmoysaha@gmail.com>
2024-09-12 20:53:11 +0600

    ACP Project NGL

---
+++ ACP ( Frequency Check).py
@@ -0,0 +1,22 @@
+
+test_dict = {
+    'Codingal': 2,
+    'is': 2,
+    'Best': 2,
+    'for': 2,
+    'Coding': 1
+}
+
+print("The Original Dictionary: " + str(test_dict))
+
+k = int(input("Enter the value to check the frequency of: "))
+
+
+res = 0
+
+for key in test_dict:
+    if test_dict[key] == k:
+        res += 1
+
+print("Frequency of " + str(k) + " is: " + str(res))
+
